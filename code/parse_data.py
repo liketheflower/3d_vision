17705df74120517fe41c012b7368836160d234f5
jimmy shen <jim.morris.shen@gmail.com>
2018-03-06 14:48:57 -0500

    add the codewq

---
+++ code/parse_data.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+from vis_ptx import plot_3d as plot_3d
+
+
+
+def file_parse(file_name,head_length):
+    line_data=[]
+    with open(file_name) as f:
+        for line in f:
+            line_data.append(line.split())
+    #print "before",head_length,line_data[0]
+    if head_length >= 1:
+        del line_data[0:head_length]
+   # print line_data[0]
+    line_data_array=np.array(line_data)
+    line_data_array = line_data_array.astype(float)
+    return line_data_array
+
+
+def get_clean_ptx(ptx_unclean):
+    # this function will remove all the ptx which is (0,0,0)
+    clean_ptx = []
+    for i in xrange(ptx_unclean.shape[0]):
+        if sum(ptx_unclean[i,:3]> 0.0001):
+            clean_ptx.append(ptx_unclean[i,:3])
+    print len(clean_ptx)
+    # the last column is reserved for the segmentation label
+    clean_ptx_array = np.zeros((len(clean_ptx),4),dtype = np.float)
+    for i in xrange(len(clean_ptx)):
+        clean_ptx_array[i,:3] = clean_ptx[i]
+    return clean_ptx_array
+def data_parser(file_name):
+
+    #get the cloud point raw data
+    cloud_point_data=file_parse(file_name, 10)
+    print cloud_point_data.shape
+    clean_ptx = get_clean_ptx(cloud_point_data)
+    print clean_ptx.shape
+    NUMBER_OF_PLOT_DATA=int(len(clean_ptx))
+    tem_file_names = file_name.split('/')
+    plot_3d(clean_ptx,tem_file_names[-1][:-4]+'.png',True)
+#    x =  clean_ptx[0:NUMBER_OF_PLOT_DATA,0]
+ #   y =  clean_ptx[0:NUMBER_OF_PLOT_DATA,1]
+  #  z =  clean_ptx[0:NUMBER_OF_PLOT_DATA,2]
+
+
+
+
+if __name__ == "__main__":
+    file_name = "./../DATA/small_example.ptx"
+    data_parser(file_name)
---
+++ code/vis_ptx.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+def plot_3d(data, file_name='small',save_fig = False):
+        #{'rear-trunk': 9, 'none': -20, 'misc': 20, 'side-right': 7, 'side-left': 6, 'roof': 1, 'front-bumper': 4, 'trunk': 3, 'interior': 8, 'rear-bumper': 5, 'hood': 2}
+    eleven_colors=['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00','#ffff33', '#a65628', '#999999','#f781bf','#bababa','#404040']
+    #ax.scatter(data[:,0], data[:,1], data[:,2], ,s=2.5, linewidth=0.1)
+    if save_fig:
+        ax = plt.axes(projection = '3d')
+        if data.shape[0]>0:
+        #ax.scatter(data[i,0], data[i,1], data[i,2], c=eleven_colors[color_lbl[i]-1], alpha=0.65)
+         #print "color_lbl[i]-1",color_lbl[i]-1
+            ax.scatter(data[:,0], data[:,1], data[:,2], alpha=0.65)
+    #ax.scatter(data[:,0], data[:,1], data[:,2], c=lbl, alpha=0.65)
+    #ax.set_xlim3d(-80, -40)
+    #ax.set_ylim3d(290, 330)
+    #ax.set_zlim3d(-36 , -32)
+    #plt.show()
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        plt.savefig('./../output/'+file_name, dpi = (200))
+        plt.show()
