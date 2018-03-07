627f3b48f3d6d6dd07ad09b011959422c2e5b021
liketheflower <jim.morris.shen@gmail.com>
2018-03-06 20:06:38 -0500

    add more codes

--- code/parse_data.py
+++ code/parse_data.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 from vis_ptx import plot_3d as plot_3d
+from numpy import linalg as LA
 
 
 
@@ -47,18 +48,88 @@ def show_one_scan_line(ptx, line_no = 100,points_per_scan_line=285):
              #print "after",one_line[i]
     plot_3d(one_line,str(line_no/points_per_scan_line)+"_to_"+str(line_no/points_per_scan_line+10)+"_scan_lines.png",True,2)
 
-def data_parser(file_name,style =0,scan_lines=99, points_per_scan_line=285):
+
+def get_center(a):
+    return np.mean(a,axis = 0)
+
+def a_min_center(a):
+    return a-get_center(a)
+
+
+def get_cov(a):
+    """
+    cov_ = [xx, xy, xz
+            yx, yy, yz
+            zx, zy, zz]
+    """
+    xx,yy,zz,xy,xz,yz = 0, 0 ,0 ,0 ,0 ,0
+    for i in xrange(len(a)):
+        x,y,z = a[i,0],a[i,1],a[i,2]
+        xx += x*x
+        yy += y*y
+        zz += z*z
+        xy += x*y
+        xz += x*z
+        yz += y*z
+    return np.array([[xx, xy, xz], 
+                     [xy, yy, yz], 
+                     [xz, yz, zz]])
+
+
+def get_norm(a):
+    A = get_cov(a)
+    w, v = LA.eig(A)
+    return v[:,np.argmin(w)]
+
+def get_clean_ptx_2d_array(a):
+    # if no point is found, return None, else return the found points in an array with the shape (#POINT,3)
+    clean_ptx =[]
+    for i in xrange(a.shape[0]):
+        for j in xrange(a.shape[1]):
+            if any(a[i,j,k]>0.00001 or a[i,j,k]<-0.0001 for k in range(3)):
+                clean_ptx.append(a[i,j])
+    if not clean_ptx: return None
+    else:
+        tem_ptx = np.zeros((len(clean_ptx), 3))
+        for ii in xrange(len(clean_ptx)):
+            tem_ptx[ii] = clean_ptx[ii]
+        return  tem_ptx
+def segmentation(ptx, K = 3, TH = 0.4):
+    # K is the neighbor width 
+    #print a_min_center(ptx)
+    R, C = ptx.shape[0], ptx.shape[1]
+    ptx_norms = np.zeros((R,C,3))
+    for i in xrange(R):
+        for j in xrange(C):
+            if i-K>=0 and j-K>=0 and i+K<R and j+K <C:
+                ptx_after_clean = get_clean_ptx_2d_array(ptx[i-K:i+K+1,j-K:j+K+1,:3])
+                if ptx_after_clean== None or ptx_after_clean.shape[0] < (2*K+1)*TH:
+                    continue
+                else:
+                    p_prime = a_min_center(ptx_after_clean)
+                    ptx_norms[i,j] = get_norm(p_prime)
+                    print ptx_norms[i,j]
+  
+    #print ptx_norms[:10,:10]
+                      
+
+def get_dot(a,b):
+    # get the dot product of a,b
+    return np.dot(a,b)
+def data_parser(file_name,style =0,scan_lines=99, points_per_scan_line=285, plot_ten_scan_lines= False):
 
     #get the cloud point raw data
     cloud_point_data = file_parse(file_name, 10)
-    for i in xrange(0,scan_lines*points_per_scan_line,int(points_per_scan_line*(0.1*scan_lines))):
-        show_one_scan_line(cloud_point_data,i,points_per_scan_line)
+    if plot_ten_scan_lines:
+        for i in xrange(0,scan_lines*points_per_scan_line,int(points_per_scan_line*(0.1*scan_lines))):
+            show_one_scan_line(cloud_point_data,i,points_per_scan_line)
     print cloud_point_data.shape
     clean_ptx = get_clean_ptx(cloud_point_data)
     print clean_ptx.shape
     NUMBER_OF_PLOT_DATA=int(len(clean_ptx))
     tem_file_names = file_name.split('/')
-    plot_3d(clean_ptx,tem_file_names[-1][:-4]+'_original.png',True,)
+    plot_3d(clean_ptx,tem_file_names[-1][:-4]+'_original.png',False,style)
+    return cloud_point_data
 #    x =  clean_ptx[0:NUMBER_OF_PLOT_DATA,0]
  #   y =  clean_ptx[0:NUMBER_OF_PLOT_DATA,1]
   #  z =  clean_ptx[0:NUMBER_OF_PLOT_DATA,2]
@@ -70,8 +141,17 @@ if __name__ == "__main__":
     file_names = ["./../DATA/small_example.ptx","./../DATA/big_example.ptx"]
     scan_lines = [99,999]
     points_per_scan_line=[285, 964]
-    for i in range(len(file_names)):
-        data_parser(file_names[i],i ,scan_lines[i],points_per_scan_line[i])
+    """
+    ptx = np.array([[0, 0, 0], 
+                    [1, 1, 1], 
+                    [2, 2, 2]])
+    segmentation(ptx)
+    """
+    for i in range(len(file_names)-1):
+        R, C = scan_lines[i],points_per_scan_line[i]
+        ptx = data_parser(file_names[i],i ,R, C)
+        ptx_reshape = ptx.reshape(R, C, ptx.shape[-1])
+        segmentation(ptx)
     
 
 
